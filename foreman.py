9499d9cf4a57743a15f235d106bbe1ee93a5c9da
tkrah <thomas.krahn@esailors.de>
2015-03-05 18:49:25 +0100

    Add missing return statements.\nAdd function get_resource_url().\nFunction get_resource() now returns the complete requests instead of just request['results'].

--- foreman.py
+++ foreman.py
@@ -6,10 +6,10 @@ Created on 04.03.2015
 
 import json
 import requests
-requests.packages.urllib3.disable_warnings()
 from requests.auth import HTTPBasicAuth
+requests.packages.urllib3.disable_warnings()
 
-FOREMAN_REQUEST_HEADERS = {'content-type': 'application/json'}
+FOREMAN_REQUEST_HEADERS = {'content-type': 'application/json', 'accept': 'application/json'}
 FOREMAN_API_VERSION = 'v2'
 
 class Foreman:
@@ -19,33 +19,45 @@ class Foreman:
         self.username = username
         self.password = password
         self.url = 'https://' + self.hostname + ':' + self.port + '/api/' + FOREMAN_API_VERSION
-        
-    def get_resource(self, resource_type):
-        try:
-            r = requests.get(url=self.url + '/' + resource_type ,
-                             auth=(self.username, self.password),
-                             verify=False)
-            if r.status_code == 200:
-                return json.loads(r.text)['results']
-            r.raise_for_status(); 
-        except:
-            raise
 
-    def put_resource(self, resource_type, resource, data):
-        try:
-            payload = {}
-            payload[resource] = data
+    def get_resource_url(self, resource_type, resoucre_id=None, action=None):
+        url = self.url + '/' + resource_type
+        if resoucre_id:
+            url = url + '/' + resoucre_id
+        if action:
+            url = url + '/' + action
+        return url
 
-            r = requests.post(url=self.url + '/' + resource_type ,
-                              data=json.dumps(payload),
-                              headers=FOREMAN_REQUEST_HEADERS,
-                              auth=(self.username, self.password),
-                              verify=False)
-            if r.status_code == 200:
-                return json.loads(r.text)            
-            r.raise_for_status()
-        except:
-            raise
+    def get_resource(self, resource_type, resource_id, action):
+        r = requests.get(url=self.get_resource_url(resource_type=resource_type),
+                         auth=(self.username, self.password),
+                         verify=False)
+        if r.status_code == requests.codes.ok:
+            return json.loads(r.text)
+        r.raise_for_status()
+
+    def post_resource(self, resource_type, resource, data):
+        payload = {}
+        payload[resource] = data
+
+        r = requests.post(url=self.get_resource_url(resource_type=resource_type) ,
+                          data=json.dumps(payload),
+                          headers=FOREMAN_REQUEST_HEADERS,
+                          auth=(self.username, self.password),
+                          verify=False)
+        if r.status_code == requests.codes.ok:
+            return json.loads(r.text)
+        r.raise_for_status()
+
+    def put_resource(self, resource_type, resource_id, data, action=None):
+        r = requests.put(url=self.get_resource_url(resource_type=resource_type, resoucre_id=resource_id, action=action),
+                         data=json.dumps(data),
+                         headers=FOREMAN_REQUEST_HEADERS,
+                         auth=(self.username, self.password),
+                         verify=False)
+        if r.status_code == requests.codes.ok:
+            return json.loads(r.text)
+        r.raise_for_status()
 
     def get_resource_by_name(self, name, list):
         for item in list:
@@ -54,7 +66,7 @@ class Foreman:
         return {}
 
     def get_architectures(self):
-        return self.get_resource(resource_type='architectures')
+        return self.get_resource(resource_type='architectures').get('results')
 
     def get_architecture_by_id(self, id):
         return self.get_resource(resource_type='architectures/' + id)
@@ -63,10 +75,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_architectures())
 
     def set_architecture(self, data):
-        self.put_resource(resource_type='architectures', resource='architecture', data=data)
+        return self.post_resource(resource_type='architectures', resource='architecture', data=data)
 
     def get_compute_resources(self):
-        return self.get_resource(resource_type='compute_resources')
+        return self.get_resource(resource_type='compute_resources').get('results')
 
     def get_compute_resource_by_id(self, id):
         return self.get_resource(resource_type='compute_resources/' + id)
@@ -75,10 +87,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_compute_resources())
 
     def set_compute_resource(self, data):
-        self.put_resource(resource_type='compute_resources', resource='compute_resource', data=data)
+        return self.post_resource(resource_type='compute_resources', resource='compute_resource', data=data)
 
     def get_compute_profiles(self):
-        return self.get_resource(resource_type='compute_profiles')
+        return self.get_resource(resource_type='compute_profiles').get('results')
 
     def get_compute_profile_by_id(self, id):
         return self.get_resource(resource_type='compute_profiles/' + id)
@@ -87,10 +99,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_compute_profiles())
 
     def set_compute_profile(self, data):
-        self.put_resource(resource_type='compute_profiles', resource='compute_profile', data=data)
+        return self.post_resource(resource_type='compute_profiles', resource='compute_profile', data=data)
 
     def get_domains(self):
-        return self.get_resource(resource_type='domains')
+        return self.get_resource(resource_type='domains').get('results')
 
     def get_domain_by_id(self, id):
         return self.get_resource(resouce_type='domains/' + id)
@@ -99,10 +111,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_domains())
 
     def set_domain(self, data):
-        self.put_resource(resource_type='domains', resource='domain', data=data)
+        return self.post_resource(resource_type='domains', resource='domain', data=data)
 
     def get_environments(self):
-        return self.get_resource(resource_type='environments')
+        return self.get_resource(resource_type='environments').get('results')
 
     def get_environment_by_id(self, id):
         return self.get_resource(resouce_type='environments/' + id)
@@ -111,22 +123,25 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_environments())
 
     def set_environment(self, data):
-        self.put_resource(resource_type='environments', resource='environment', data=data)
+        return self.post_resource(resource_type='environments', resource='environment', data=data)
 
     def get_hosts(self):
-        return self.get_resource(resource_type='hosts')
+        return self.get_resource(resource_type='hosts').get('results')
 
     def get_host_by_id(self, id):
-        return self.get_resource(resouce_type='hosts/' + id)
+        return self.get_resource(resource_type='hosts/' + id)
 
     def get_host_by_name(self, name):
         return self.get_resource_by_name(name=name, list=self.get_hosts())
 
     def set_host(self, data):
-        self.put_resource(resource_type='hosts', resource='host', data=data)
+        return self.post_resource(resource_type='hosts', resource='host', data=data)
+
+    def set_host_power(self, host_id, action):
+        return self.put_resource(resource_type='hosts', resource_id=host_id, action='power', data={'power_action': 'status', 'host': {}})
 
     def get_hostgroups(self):
-        return self.get_resource(resource_type='hostgroups')
+        return self.get_resource(resource_type='hostgroups').get('results')
 
     def get_hostgroup_by_id(self, id):
         return self.get_resource(resouce_type='hostgroups/' + id)
@@ -135,10 +150,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_hostgroups())
 
     def set_hostgroup(self, data):
-        self.put_resource(resource_type='hostgroups', resource='hostgroup', data=data)
+        return self.post_resource(resource_type='hostgroups', resource='hostgroup', data=data)
 
     def get_locations(self):
-        return self.get_resource(resource_type='locations')
+        return self.get_resource(resource_type='locations').get('results')
 
     def get_location_by_id(self, id):
         return self.get_resource(resouce_type='locations/' + id)
@@ -147,10 +162,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_locations())
 
     def set_location(self, data):
-        self.put_resource(resource_type='locations', resource='location', data=data)
+        return self.post_resource(resource_type='locations', resource='location', data=data)
 
     def get_medias(self):
-        return self.get_resource(resource_type='media')
+        return self.get_resource(resource_type='media').get('results')
 
     def get_media_by_id(self, id):
         return self.get_resource(resouce_type='media/' + id)
@@ -159,10 +174,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_medias())
 
     def set_media(self, data):
-        self.put_resource(resource_type='media', resource='medium', data=data)
+        return self.post_resource(resource_type='media', resource='medium', data=data)
 
     def get_organizations(self):
-        return self.get_resource(resource_type='organizations')
+        return self.get_resource(resource_type='organizations').get('results')
 
     def get_organization_by_id(self, id):
         return self.get_resource(resouce_type='organizations/' + id)
@@ -171,10 +186,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_organizations())
 
     def set_organization(self, data):
-        self.put_resource(resource_type='organizations', resource='organization', data=data)
+        return self.post_resource(resource_type='organizations', resource='organization', data=data)
 
     def get_operatingsystems(self):
-        return self.get_resource(resource_type='operatingsystems')
+        return self.get_resource(resource_type='operatingsystems').get('results')
 
     def get_operatingsystem_by_id(self, id):
         return self.get_resource(resouce_type='operatingsystems/' + id)
@@ -183,10 +198,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_operatingsystems())
 
     def set_operatingsystem(self, data):
-        self.put_resource(resource_type='operatingsystems', resource='operatingsystem', data=data)
+        return self.post_resource(resource_type='operatingsystems', resource='operatingsystem', data=data)
 
     def get_partition_tables(self):
-        return self.get_resource(resource_type='ptables')
+        return self.get_resource(resource_type='ptables').get('results')
 
     def get_partition_table_by_id(self, id):
         return self.get_resource(resouce_type='ptables/' + id)
@@ -195,10 +210,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_partition_tables())
 
     def set_partition_table(self, data):
-        self.put_resource(resource_type='ptables', resource='ptable', data=data)
+        return self.post_resource(resource_type='ptables', resource='ptable', data=data)
 
     def get_smart_proxies(self):
-        return self.get_resource(resource_type='smart_proxies')
+        return self.get_resource(resource_type='smart_proxies').get('results')
 
     def get_smart_proxy_by_id(self, id):
         return self.get_resource(resouce_type='smart_proxies/' + id)
@@ -207,10 +222,10 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_smart_proxies())
 
     def set_smart_proxy(self, data):
-        self.put_resource(resource_type='smart_proxies', resource='smart_proxy', data=data)
+        return self.post_resource(resource_type='smart_proxies', resource='smart_proxy', data=data)
 
     def get_subnets(self):
-        return self.get_resource(resource_type='subnets')
+        return self.get_resource(resource_type='subnets').get('results')
 
     def get_subnet_by_id(self, id):
         return self.get_resource(resouce_type='subnets/' + id)
@@ -219,4 +234,4 @@ class Foreman:
         return self.get_resource_by_name(name=name, list=self.get_subnets())
 
     def set_subnet(self, data):
-        self.put_resource(resource_type='subnets', resource='subnet', data=data)
+        return self.post_resource(resource_type='subnets', resource='subnet', data=data)
