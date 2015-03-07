a3f91231b0c4cba7cb1dddadd3f8010d78269bba
Nosmoht <ntbc@gmx.net>
2015-03-07 10:02:24 +0100

    Add description and install requirements

--- setup.py
+++ setup.py
@@ -3,11 +3,24 @@
 
 from distutils.core import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='python-foreman',
       version='0.3',
+      description='Python class to communicate with Foreman via API v2',
+      long_description=readme(),
+      keywords=['foreman'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+      ],
       author='Thomas Krahn',
       author_email='ntbc@gmx.net',
       license='MIT',
       url='https://github.com/Nosmoht/python-foreman',
       packages=['foreman'],
+      install_requires=['python-requests'],
       )
