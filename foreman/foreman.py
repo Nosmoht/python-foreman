4cdd0cbd297040af2df894f7485247dbb33a4785
Nosmoht <ntbc@gmx.net>
2015-03-22 23:25:22 +0100

    Bring back constants

--- foreman/foreman.py
+++ foreman/foreman.py
@@ -6,9 +6,49 @@ Created on 04.03.2015
 
 import json
 import requests
-from foreman.constants import *
 # from requests.auth import HTTPBasicAuth
 requests.packages.urllib3.disable_warnings()
+
+FOREMAN_REQUEST_HEADERS = {
+    'content-type': 'application/json',
+    'accept': 'application/json'
+}
+FOREMAN_API_VERSION = 'v2'
+
+ARCHITECTURES = 'architectures'
+ARCHITECTURE = 'architecture'
+COMMON_PARAMETERS = 'common_parameters'
+COMMON_PARAMETER = 'common_parameter'
+COMPUTE_ATTRIBUTES = 'compute_attributes'
+COMPUTE_ATTRIBUTE = 'compute_attribute'
+COMPUTE_PROFILES = 'compute_profiles'
+COMPUTE_PROFILE = 'compute_profile'
+COMPUTE_RESOURCES = 'compute_resources'
+COMPUTE_RESOURCE = 'compute_resource'
+CONFIG_TEMPLATES = 'config_templates'
+CONFIG_TEMPLATE = 'config_template'
+DOMAINS = 'domains'
+DOMAIN = 'domain'
+ENVIRONMENTS = 'environments'
+ENVIRONMENT = 'environment'
+HOSTS = 'hosts'
+HOST = 'host'
+HOSTGROUPS = 'hostgroups'
+HOSTGROUP = 'hostgroups'
+LOCATIONS = 'locations'
+LOCATION = 'location'
+MEDIA = 'media'
+MEDIUM = 'medium'
+OPERATINGSYSTEMS = 'operatingsystems'
+OPERATINGSYSTEM = 'operatingsystem'
+ORGANIZATIONS = 'organizations'
+ORGANIZATION = 'organization'
+PARTITION_TABLES = 'ptables'
+PARTITION_TABLE = 'ptable'
+SMART_PROXIES = 'smart_proxies'
+SMART_PROXY = 'smart_proxy'
+SUBNETS = 'subnets'
+SUBNET = 'subnet'
 
 class ForemanError(Exception):
     """ForemanError Class
